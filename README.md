6e4598714a70ea89e720177b983afebfed0ccf18
Dmitry Voronin <developer.dmitry.voronin@gmail.com>
2020-10-05 11:38:22 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Elementary JS site constructor
+
+## Build Setup
+
+```bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:4200
+$ npm run serve
+
+# build for production and launch server
+$ npm run build
+```
---
+++ src/model.js
@@ -0,0 +1,56 @@
+import image from "./assets/image.jpg";
+
+import {
+  TextBlock,
+  TitleBlock,
+  ColumnsBlock,
+  ImageBlock,
+} from "./classes/blocks";
+
+const text = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio molestias dolor iste blanditiis, animi repellendus.`;
+
+export const model = [
+  new TitleBlock("Конструктор сайтов на чистом JavaScript", {
+    tag: "h2",
+    styles: {
+      background: "linear-gradient(to right, #ff0000, #ffff00)",
+      color: "#000000",
+      padding: "1.5rem",
+      "text-align": "center",
+    },
+  }),
+  new ImageBlock(image, {
+    styles: {
+      padding: "2rem 0",
+      display: "flex",
+      "justify-content": "center",
+    },
+    imageStyles: {
+      width: "500px",
+      height: "auto",
+    },
+    alt: "This is cat",
+  }),
+  new ColumnsBlock(
+    [
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, sed.",
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, sed.",
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, sed.",
+    ],
+    {
+      styles: {
+        background: "linear-gradient(to bottom, #8e2de2, #4a00e0)",
+        padding: "2rem",
+        color: "#fff",
+        "font-weight": "bold",
+      },
+    }
+  ),
+  new TextBlock(text, {
+    styles: {
+      background: "linear-gradient(to left, #f2994a, #f2c94c)",
+      padding: "1rem",
+      "font-weight": "bold",
+    },
+  }),
+];
